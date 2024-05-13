a4c4dc51aefcc32817ce6aaed5e74133b935f256
Manjil Rajbhandari <manjilrajbhandari@gmail.com>
2024-05-13 06:55:49 +0545

    Initial Commit

---
+++ P2_GuessFlag_iOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P2_GuessFlag_iOS17
+//
+//  Created by manjil rajbhandari on 13/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P2_GuessFlag_iOS17/P2_GuessFlag_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  P2_GuessFlag_iOS17App.swift
+//  P2_GuessFlag_iOS17
+//
+//  Created by manjil rajbhandari on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct P2_GuessFlag_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
