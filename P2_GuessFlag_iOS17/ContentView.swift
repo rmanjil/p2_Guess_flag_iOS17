3fbf4e7268a2a05490354c8080cf956d03b3d109
Manjil Rajbhandari <manjilrajbhandari@gmail.com>
2024-06-12 06:58:11 +0545

    main: feat: different way of using button

--- P2_GuessFlag_iOS17/ContentView.swift
+++ P2_GuessFlag_iOS17/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State private var showAlert = false
     var body: some View {
-        Button("Delete selection", role: .destructive, action: executeDelete)
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+            
+        }
     }
     
     func executeDelete() {
