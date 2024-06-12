8071b797703d76111ab9d00951098419ebc12f24
Manjil Rajbhandari <manjilrajbhandari@gmail.com>
2024-06-12 06:39:32 +0545

    main: feat: use  LinearGradient with stops

--- P2_GuessFlag_iOS17/ContentView.swift
+++ P2_GuessFlag_iOS17/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showAlert = false
     var body: some View {
-        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+        LinearGradient(stops: [Gradient.Stop(color: .white, location: 0.45), .init(color: .black, location: 0.55)],  startPoint: .top, endPoint: .bottom)
     }
 }
 
