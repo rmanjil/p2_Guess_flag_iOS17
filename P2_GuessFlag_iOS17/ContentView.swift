ad07bf45cf86bceea2f9592e11de74a95e98ab22
Manjil Rajbhandari <manjilrajbhandari@gmail.com>
2024-06-24 06:19:38 +0545

    main: feat: add flag image and show image  in content view

--- P2_GuessFlag_iOS17/ContentView.swift
+++ P2_GuessFlag_iOS17/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showAlert = false
+    let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Button {
-            print("Button is Tapped")
-        } label: {
-            HStack {
-                Text("Edit")
-                Image(systemName: "pencil")
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[number])
+                    }
+                }
             }
         }
     }
